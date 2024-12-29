dcb86f41f8a8a00b5fe2ab94f26aeea46c66ecf0
Carlos Cauã <137356546+Schatten900@users.noreply.github.com>
2024-12-29 13:47:50 -0300

    Update main.jsx
    Retirando tudo vinculado ao App.jsx

--- proj-rc/frontend/workstream/src/main.jsx
+++ proj-rc/frontend/workstream/src/main.jsx
@@ -2,15 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './styles/index.css'
-import App from './App.jsx'
 import Login from './pages/Login.jsx';
 
 const router = createBrowserRouter([
   //Rotas possiveis na aplicação
-  {
-    path: "/",
-    element: <App />,
-  },
   {
     path:"/login",
     element: <Login/>
