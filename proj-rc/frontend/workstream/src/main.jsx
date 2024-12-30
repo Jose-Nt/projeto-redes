ee3857970d478bbef38a92eb3f90b2a448ca6cb5
Schatten900 <carloscaua.silva9@gmail.com>
2024-12-30 15:09:41 -0300

    teste

--- proj-rc/frontend/workstream/src/main.jsx
+++ proj-rc/frontend/workstream/src/main.jsx
@@ -1,25 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import './styles/index.css'
-import App from './App.jsx'
-import Login from './pages/Login.jsx';
+import "./styles/index.css";
+import Login from "./pages/Login.jsx";
 
 const router = createBrowserRouter([
   //Rotas possiveis na aplicação
   {
-    path: "/",
-    element: <App />,
+    path: "/login",
+    element: <Login />,
   },
-  {
-    path:"/login",
-    element: <Login/>
-  }
-])
+]);
 
 //Renderiza as rotas da aplicação
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router = {router}/> 
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
